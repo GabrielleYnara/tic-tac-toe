9e82439cff56feba93c118dd547ce534dfc11e2b
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-08-16 20:37:16 -0500

    Added winning rules and reset the board after a match

--- js/game.js
+++ js/game.js
@@ -34,6 +34,105 @@ class Game {
         element.target.append(p);
         this.activeTurn = this.players.find(player => player.name != this.activeTurn.name);
         document.querySelector(".active-player").innerHTML = `${this.activeTurn.icon} is playing`;
+        let result = this.checkWinner();
+        if(result){
+            alert(`${result.icon} Won!`)
+            this.resetBoard();
+        }
+    }
+
+    checkWinner() {
+        const positions = document.querySelectorAll(".board-game div");
+
+        //Top Row
+        if(positions[0].children.length && positions[1].children.length && positions[2].children.length){ // Make sure there's a child tag to handle
+            if (positions[0].children[0].innerHTML === positions[1].children[0].innerHTML && 
+                positions[0].children[0].innerHTML === positions[2].children[0].innerHTML) {
+                let winner = this.players.find(player => {
+                    return player.icon === positions[0].children[0].innerHTML;
+                });
+                return winner;
+            }
+        }
+        //Middle Row
+        if(positions[3].children.length && positions[4].children.length && positions[5].children.length){ // Make sure there's a child tag to handle
+            if (positions[3].children[0].innerHTML === positions[4].children[0].innerHTML && 
+                positions[3].children[0].innerHTML === positions[5].children[0].innerHTML) {
+                let winner = this.players.find(player => {
+                    return player.icon === positions[3].children[0].innerHTML;
+                });
+                return winner;
+            }
+        }
+        //Bottom Row
+        if(positions[6].children.length && positions[7].children.length && positions[8].children.length){ // Make sure there's a child tag to handle
+            if (positions[6].children[0].innerHTML === positions[7].children[0].innerHTML && 
+                positions[6].children[0].innerHTML === positions[8].children[0].innerHTML) {
+                let winner = this.players.find(player => {
+                    return player.icon === positions[6].children[0].innerHTML;
+                });
+                return winner;
+            }
+        }
+        //First Column
+        if(positions[0].children.length && positions[3].children.length && positions[6].children.length){ // Make sure there's a child tag to handle
+            if (positions[0].children[0].innerHTML === positions[3].children[0].innerHTML && 
+                positions[0].children[0].innerHTML === positions[6].children[0].innerHTML) {
+                let winner = this.players.find(player => {
+                    return player.icon === positions[0].children[0].innerHTML;
+                });
+                return winner;
+            }
+        }
+        //Second Column
+        if(positions[1].children.length && positions[4].children.length && positions[7].children.length){ // Make sure there's a child tag to handle
+            if (positions[1].children[0].innerHTML === positions[4].children[0].innerHTML && 
+                positions[1].children[0].innerHTML === positions[7].children[0].innerHTML) {
+                let winner = this.players.find(player => {
+                    return player.icon === positions[1].children[0].innerHTML;
+                });
+                return winner;
+            }
+        }
+        //Third Column
+        if(positions[2].children.length && positions[5].children.length && positions[8].children.length){ // Make sure there's a child tag to handle
+            if (positions[2].children[0].innerHTML === positions[5].children[0].innerHTML && 
+                positions[2].children[0].innerHTML === positions[8].children[0].innerHTML) {
+                let winner = this.players.find(player => {
+                    return player.icon === positions[2].children[0].innerHTML;
+                });
+                return winner;
+            }
+        }
+        //Diagonal 159
+        if(positions[0].children.length && positions[4].children.length && positions[8].children.length){ // Make sure there's a child tag to handle
+            if (positions[0].children[0].innerHTML === positions[4].children[0].innerHTML && 
+                positions[0].children[0].innerHTML === positions[8].children[0].innerHTML) {
+                let winner = this.players.find(player => {
+                    return player.icon === positions[0].children[0].innerHTML;
+                });
+                return winner;
+            }
+        }
+        //Counter-Diagonal 753
+        if(positions[2].children.length && positions[4].children.length && positions[6].children.length){ // Make sure there's a child tag to handle
+            if (positions[2].children[0].innerHTML === positions[4].children[0].innerHTML && 
+                positions[2].children[0].innerHTML === positions[6].children[0].innerHTML) {
+                let winner = this.players.find(player => {
+                    return player.icon === positions[2].children[0].innerHTML;
+                });
+                return winner;
+            }
+        }
+    }
+    resetBoard(){
+        const positions = document.querySelectorAll(".board-game div");
+        positions.forEach(element => {
+            if(element.children.length){ //if it was populated
+                element.innerHTML = "";
+            }
+            console.log(element);
+        })
     }
 }
 
